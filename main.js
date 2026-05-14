55110fbbf8a5b8c703da41ead5c581abdb18a7f0
supermanone-boop <daikihashi1127@gmail.com>
2026-05-14 18:38:48 +0900

    Add 50M Collision Zone object to geofs

---
+++ main.js
@@ -0,0 +1,107 @@
+'use strict';
+
+(() => {
+
+    
+
+    const CENTER_LAT = 35.689207296197;
+    const CENTER_LON = 139.69175111;
+    const CENTER_ALT = 282.8;
+
+   
+
+    const R = 50;
+    const SEG = 12;
+
+    
+
+    const ring = [];
+
+    for(let i=0;i<SEG;i++){
+
+        const a = (i / SEG) * Math.PI * 2;
+
+        ring.push([
+
+            Math.cos(a) * R,
+            Math.sin(a) * R,
+            0
+
+        ]);
+    }
+
+
+
+    function tri(p0,p1,p2){
+
+        const u = [
+            p1[0]-p0[0],
+            p1[1]-p0[1],
+            p1[2]-p0[2]
+        ];
+
+        const v = [
+            p2[0]-p0[0],
+            p2[1]-p0[1],
+            p2[2]-p0[2]
+        ];
+
+        const n = [
+            u[1]*v[2] - u[2]*v[1],
+            u[2]*v[0] - u[0]*v[2],
+            u[0]*v[1] - u[1]*v[0]
+        ];
+
+        return Object.assign([p0,p1,p2], {u,v,n});
+    }
+
+    const collisionTriangles = [];
+
+    for(let i=1;i<SEG-1;i++){
+
+        collisionTriangles.push(
+            tri(ring[0], ring[i], ring[i+1])
+        );
+    }
+
+    
+
+    const obj = {
+
+        name: "50M Collision Zone",
+
+        type: 100,
+
+        url: "https://raw.githubusercontent.com/supermanone-boop/models/main/a10c.glb",
+
+        location: [
+            CENTER_LAT,
+            CENTER_LON,
+            CENTER_ALT
+        ],
+
+        htr: [0,0,0],
+
+        rotateModelOnly: false,
+
+        scale: 1,
+
+        metricOffset: [0,0,0],
+
+        collisionRadius: 120,
+
+        collisionTriangles,
+
+        options: {}
+
+    };
+
+   
+
+    geofs.objects.objectList.push(obj);
+
+    geofs.objects.loadModels();
+
+    console.log("[50M COLLISION ZONE] spawned");
+
+})();
