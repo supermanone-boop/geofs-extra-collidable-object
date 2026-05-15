5db0824e2043978deff81b79185547c72f7cd5a2
supermanone-boop <daikihashi1127@gmail.com>
2026-05-15 13:50:05 +0900

    groundaircraft.js の更新

--- groundaircraft.js
+++ groundaircraft.js
@@ -2,49 +2,80 @@
 
 (() => {
 
-    const LAT = 34.617103901204814;
-    const LON = 134.7943127399184;
-    const ALT = 183.40703763074646;
+    // =========================
+    // 機体リスト
+    // =========================
+    const OBJECTS = [
 
-    const url =
-      "https://www.geo-fs.com/backend/aircraft/repository/747-8F%20by%20JAaMDG%20and%20Boa93_364320_5409/74D2.glb";
+        {
+            name: "A380",
+            lla: [34.617293779346646, 134.80097227352553, 181.9825137569666],
+            url: "https://www.geo-fs.com/models/aircraft/premium/a380/a380.gltf",
+            scale: 1,
+            minimumPixelSize: 0
+        },
 
-    function spawn747() {
+        {
+            name: "772",
+            lla: [34.617373460151356, 134.7977016109173, 181.98268817287962],
+            url: "https://www.geo-fs.com/backend/aircraft/repository/Protium%20H1_448101_4016/772-3.glb",
+            scale: 1,
+            minimumPixelSize: 0
+        },
+
+        {
+            name: "747-8F",
+            lla: [34.617103901204814, 134.7943127399184, 183.40703763074646],
+            url: "https://www.geo-fs.com/backend/aircraft/repository/747-8F%20by%20JAaMDG%20and%20Boa93_364320_5409/74D2.glb",
+            scale: 1,
+            minimumPixelSize: 0
+        }
+
+    ];
+
+    // =========================
+    // spawn関数
+    // =========================
+    function spawn(obj) {
 
         const viewer = geofs.api.viewer;
 
         const pos = Cesium.Cartesian3.fromDegrees(
-            LON,
-            LAT,
-            ALT
+            obj.lla[1],
+            obj.lla[0],
+            obj.lla[2]
         );
 
         const model = viewer.scene.primitives.add(
             Cesium.Model.fromGltf({
 
-                url: url,
+                url: obj.url,
 
                 modelMatrix:
                     Cesium.Transforms.eastNorthUpToFixedFrame(pos),
 
-                scale: 1,
+                scale: obj.scale,
 
-                minimumPixelSize: 128
+                minimumPixelSize: obj.minimumPixelSize
 
             })
         );
 
-        console.log("[747-8F] spawned");
+        console.log(`[${obj.name}] spawned`);
 
         return model;
     }
 
+    // =========================
+    // 起動待ち
+    // =========================
     const wait = setInterval(() => {
 
         if (window.geofs?.api?.viewer && window.Cesium) {
 
             clearInterval(wait);
-            spawn747();
+
+            OBJECTS.forEach(spawn);
 
         }
 
